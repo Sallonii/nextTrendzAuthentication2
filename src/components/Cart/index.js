f8135d224da2fae1c31e11b97f67562528f5f1ae
Saloni <salonichandra8955@gmail.com>
2024-02-15 05:16:44 +0000

    initial commit

---
+++ src/components/Cart/index.js
@@ -0,0 +1,19 @@
+// Write your JS code here
+import Header from '../Header'
+
+import './index.css'
+
+const Cart = () => (
+  <>
+    <Header />
+    <div className="cart-section">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+        alt="cart"
+        className="image"
+      />
+    </div>
+  </>
+)
+
+export default Cart
